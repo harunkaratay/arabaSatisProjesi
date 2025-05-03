92c6257c3b922434b097610eb78c619feb785c96
harunkaratay <harunkaratayy@outlook.com>
2025-05-04 02:58:59 +0300

    car model modeli oluşturuldu

---
+++ app/Models/CarModel.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class CarModel extends Model
+{
+    use HasFactory, softDeletes;
+    protected $table = 'car_models';
+
+    public function getBrand() {
+        return $this->belongsTo(CarBrand::class, 'brand_id', 'id');
+    }
+}
